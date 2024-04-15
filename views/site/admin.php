9b3aa2b2b1e8f02fcd6a0892cb0cfb32fdf7cf23
79234 <marusya0323@gmail.com>
2024-04-15 11:14:41 +0700

    Создание панели админа

--- views/site/admin.php
+++ views/site/admin.php
@@ -1,8 +1,11 @@
-<h2>Добавление сотрудника</h2>
-<h3><?= $message ?? ''; ?></h3>
-<form method="post">
+<div class="add-user-container">
+    <h2>Добавление сотрудника</h2>
+    <h3><?= $message ?? ''; ?></h3>
+    <form method="post">
 
-    <label>Логин <input type="text" name="login"></label>
-    <label>Пароль <input type="password" name="password"></label>
-    <button>Зарегистрироваться</button>
-</form>
+        <label>Логин <input type="text" name="login"></label>
+        <label>Пароль <input type="password" name="password"></label>
+        <button>Добавить</button>
+    </form>
+
+</div>
